c488161491cd3f347a3ade7cf494202bab6c38cd
deliriummary <deliriummary@gmail.com>
2020-09-24 21:46:10 -0700

    ninth

--- src/App2.js
+++ src/App2.js
@@ -10,7 +10,7 @@ export default class App2 extends Component {
 
 
     componentDidMount() {
-    axios.get('http://rallendalle.pythonanywhere.com:8000/api/')
+    axios.get('/api/')
       .then(res => {
         return this.setState({
           things: res.data
